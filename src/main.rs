563a268988f99147b4f38f0df0dc9b5b5e4b4a5b
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-09-16 15:48:05 +0200

    Create glfw window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate glfw;
+
+use glfw::{Action, Context, Key};
+
 fn main() {
-    println!("Hello World");
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    let (mut window, events) = glfw.create_window(400, 300, "Hello This is window", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window");
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    while !window.should_close() {
+        window.swap_buffers();
+
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            handle_window_event(&mut window, event);
+        }
+    }
+}
+
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    match event {
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+            window.set_should_close(true)
+        }
+        _ => {}
+    }
 }
