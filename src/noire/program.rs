a96e930602667a41e729889e2271bb19c0f76312
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2017-09-18 23:35:59 +0200

    Find all uniforms in Program

--- src/noire/program.rs
+++ src/noire/program.rs
@@ -2,16 +2,27 @@
 extern crate gl;
 
 use self::gl::types::*;
+
+use std::collections::HashMap;
 use std::ptr;
 use std::str;
 
 use noire::shader::Shader;
 
 #[derive(Debug)]
+struct Variable {
+    name: String,
+    data_type: u32,
+    size: i32,
+    location: i32,
+}
+
+#[derive(Debug)]
 pub struct Program {
-    pub vertex_shader: Shader,
-    pub pixel_shader: Shader,
+    vertex_shader: Shader,
+    pixel_shader: Shader,
     pub id: u32,
+    uniforms: HashMap<String, Variable>,
 }
 
 fn get_link_error(program: u32) -> String {
@@ -38,6 +49,58 @@ fn get_link_error(program: u32) -> String {
     log_text
 }
 
+fn find_uniforms(program: u32) -> HashMap<String, Variable> {
+    let mut result = HashMap::new();
+    unsafe {
+        let mut num_uniforms = 0;
+        let mut max_uniform_length = 0;
+
+        gl::GetProgramiv(program, gl::ACTIVE_UNIFORMS, &mut num_uniforms);
+        gl::GetProgramiv(
+            program,
+            gl::ACTIVE_UNIFORM_MAX_LENGTH,
+            &mut max_uniform_length,
+        );
+
+        // initialize char buffer with length
+        let mut buffer = Vec::with_capacity(max_uniform_length as usize);
+        buffer.set_len((max_uniform_length as usize) - 1);
+
+        for i in 0..num_uniforms {
+            let mut length = 0;
+            let mut uniform_size: i32 = 0;
+            let mut uniform_type = gl::FLOAT;
+            let uniform_location =
+                gl::GetAttribLocation(program, buffer.as_mut_ptr() as *mut GLchar);
+
+            gl::GetActiveUniform(
+                program,
+                i as u32,
+                max_uniform_length,
+                &mut length,
+                &mut uniform_size,
+                &mut uniform_type,
+                buffer.as_mut_ptr() as *mut GLchar,
+            );
+
+            let uniform_name = str::from_utf8(&buffer)
+                .ok()
+                .expect("GetActiveUniform not valid utf8")
+                .to_string();
+
+            let uniform: Variable = Variable {
+                name: uniform_name.clone(),
+                location: uniform_location,
+                data_type: uniform_type,
+                size: uniform_size,
+            };
+
+            result.insert(uniform_name, uniform);
+        }
+    }
+    result
+}
+
 pub fn link_program(vertex_shader: Shader, pixel_shader: Shader) -> Result<Program, String> {
     let id;
     unsafe {
@@ -58,6 +121,7 @@ pub fn link_program(vertex_shader: Shader, pixel_shader: Shader) -> Result<Progr
         vertex_shader: vertex_shader,
         pixel_shader: pixel_shader,
         id: id,
+        uniforms: find_uniforms(id),
     };
     Ok(program)
 }
