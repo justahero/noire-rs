8a441bca4cab73acff8e18f8c160dc6e8a5e66bd
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2019-06-09 16:03:52 +0200

    Extend Spotlight struct
    * set light depth texture
    * extend spotlight example

--- examples/04-spotlight/main.rs
+++ examples/04-spotlight/main.rs
@@ -14,8 +14,11 @@ use cgmath::*;
 use noire::math::*;
 use noire::math::{Camera, Color};
 use noire::mesh::{Cube, Mesh, Plane};
-use noire::render::{Bindable, Capability, Drawable, Program, Shader, VertexArrayObject};
+use noire::render::opengl::get_render_error;
+use noire::render::{Bindable, Drawable, FrameBuffer, Program, Shader, Texture, VertexArrayObject};
+use noire::render::{Capability, Primitive, Size};
 use noire::render::{OpenGLWindow, RenderWindow, Window};
+use noire::render::spot_light::*;
 
 use notify::*;
 use std::sync::mpsc::channel;
@@ -34,6 +37,8 @@ fn main() {
     let fragment_file = String::from("./examples/04-spotlight/shaders/fragment.glsl");
     let mut program: Program = Program::compile_from_files(&vertex_file, &fragment_file).unwrap();
 
+    let light_pos = point3(-2.5, 0.0, 1.0);
+
     // create vertex data
     let cube = Mesh::create_cube(Cube::create(0.75)).unwrap();
     let mut plane = Mesh::create_plane(Plane::create(10.0, 10.0)).unwrap();
@@ -43,11 +48,41 @@ fn main() {
     camera
         .perspective(60.0, window.aspect(), 0.1, 80.0)
         .lookat(
-            point3(0.0, 1.0, -2.5),
+            point3(0.0, 4.0, -3.5),
             point3(0.0, 0.0, 0.0),
             vec3(0.0, 1.0, 0.0)
         );
-    let light_pos = vec3(-2.5, 0.0, 1.0);
+
+    let light_pos = point3(-2.5, 0.0, 1.0);
+
+    let mut spot_light = Spotlight::new();
+    spot_light.set_lookat(
+        light_pos.clone(),
+        point3(0.5, 0.0, 1.0),
+        vec3(0.0, 1.0, 0.0),
+    );
+
+    // Textures & Frame Buffers
+    let light_texture_size = Size{ width: 1024, height: 1024 };
+    let mut light_texture = Texture::create2d().unwrap();
+    light_texture.bind();
+    light_texture.set_size(&light_texture_size).unwrap();
+    light_texture.clamp_to_edge();
+    light_texture.nearest();
+    light_texture.unbind();
+
+    let mut light_depth_texture = Texture::create_depth_texture().unwrap();
+    light_depth_texture.bind();
+    light_depth_texture.set_size(&light_texture_size).unwrap();
+    light_depth_texture.clamp_to_edge();
+    light_depth_texture.nearest();
+    light_depth_texture.unbind();
+
+    let mut light_frame_buffer = FrameBuffer::create().unwrap();
+    light_frame_buffer.bind();
+    light_frame_buffer.set_texture(&light_texture).expect("Set texture failed");
+    light_frame_buffer.set_depth_buffer(&light_depth_texture).expect("Set depth buffer failed");
+    light_frame_buffer.unbind();
 
     let start_time = Instant::now();
 
@@ -78,7 +113,7 @@ fn main() {
             .uniform("u_modelViewProjection", model_view_proj.into())
             .uniform("u_normalMatrix", normal_matrix.into())
             .uniform("u_objectColor", Color::rgb(0.4, 0.8, 0.25).into())
-            .uniform("u_shininess", 4.0.into());
+            .uniform("u_shininess", 64.0.into());
 
         plane.vao.bind();
         plane.vao.draw();
@@ -87,15 +122,16 @@ fn main() {
         // animate and render plane
         let rotate_x = Matrix4::from_angle_x(Rad::from(Deg(elapsed * 22.5)));
         let rotate_y = Matrix4::from_angle_y(Rad::from(Deg(elapsed * 45.0)));
-        let model_view = camera.view * cube.model_view * rotate_y * rotate_x;;
+
+        let model_view = camera.view * cube.model_view * rotate_y * rotate_x;
         let model_view_proj = camera.projection * model_view;
         let normal_matrix: Matrix3<f32> = convert_to_matrix3(&model_view).invert().unwrap().transpose();
 
         program.uniform("u_modelView", model_view.into());
         program.uniform("u_modelViewProjection", model_view_proj.into());
         program.uniform("u_normalMatrix", normal_matrix.into());
-        program.uniform("u_objectColor", Color::rgb(1.0, 1.0, 1.0).into());
-        program.uniform("u_shininess", 32.0.into());
+        program.uniform("u_objectColor", Color::rgb(0.2, 0.5, 0.95).into());
+        program.uniform("u_shininess", 16.0.into());
 
         cube.vao.bind();
         cube.vao.draw();
--- src/render/spot_light.rs
+++ src/render/spot_light.rs
@@ -1,30 +1,45 @@
-use cgmath::One;
-use cgmath::{Matrix4, Vector3};
+use cgmath::prelude::InnerSpace;
+use cgmath::Transform;
+use cgmath::{Matrix4, Point3, Vector3};
 
 use super::Perspective;
 
 pub struct Spotlight {
-    pub projection: Matrix4<f32>,
     pub view: Matrix4<f32>,
     pub perspective: Perspective,
+    pub pos: Point3<f32>,
+    pub target: Point3<f32>,
+    pub direction: Vector3<f32>,
+}
+
+fn get_direction(eye: &Point3<f32>, target: &Point3<f32>) -> Vector3<f32> {
+    (eye - target).normalize()
 }
 
 impl Spotlight {
     pub fn new() -> Self {
+        let pos = Point3{ x: 0.0, y: 0.0, z: 0.0 };
+        let target = Point3{ x: -1.0, y: 0.0, z: 0.0 };
+
         Spotlight {
-            projection: Matrix4::one(),
             view: Matrix4::one(),
             perspective: Perspective::default(),
+            pos,
+            target,
+            direction: get_direction(&pos, &target),
         }
-    }
-
-    pub fn translate(&mut self, pos: Vector3<f32>) -> &mut Self {
-        self.view = self.view * Matrix4::from_translation(pos);
-        self
     }
 
     pub fn set_perspective(&mut self, perspective: Perspective) -> &mut Self {
         self.perspective = perspective;
+        self
+    }
+
+    pub fn set_lookat(&mut self, eye: Point3<f32>, target: Point3<f32>, up: Vector3<f32>) -> &mut Self {
+        self.pos       = eye;
+        self.target    = target;
+        self.direction = get_direction(&self.pos, &self.target);
+        self.view      = Matrix4::look_at(eye, target, up);
         self
     }
 }
