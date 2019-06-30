2158a84678d98d19bc5d40da0975bd5b1d812be7
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2019-06-30 14:20:19 +0200

    Refactor spotlight example
    * display cube over plane, clear background correctly
    * refactor use of variables
    * add error type to create mesh functions
    * refactor VertexArrayObject to use method chaining
    * adjust examples

--- src/mesh/mesh.rs
+++ src/mesh/mesh.rs
@@ -1,10 +1,10 @@
-use cgmath::Matrix4;
+use cgmath::{Matrix4, Vector3};
 use cgmath::One;
 
 use super::cube::Cube;
 use super::plane::Plane;
 
-use render::Primitive;
+use render::{Primitive, RenderError};
 use render::vertex::VertexArrayObject;
 use render::vertex_buffer::VertexBuffer;
 use render::index_buffer::IndexBuffer;
@@ -17,32 +17,38 @@ pub struct Mesh {
 
 impl Mesh {
     /// Creates a Mesh object from a Cube
-    pub fn create_cube(cube: Cube) -> Mesh {
-        let mut vao = VertexArrayObject::new();
+    pub fn create_cube(cube: Cube) -> Result<Mesh, RenderError> {
+        let mut vao = VertexArrayObject::new()?;
 
         vao.add_vb(VertexBuffer::create(&cube.vertices, 3, Primitive::Triangles));
         vao.add_vb(VertexBuffer::create(&cube.normals, 3, Primitive::Triangles));
         vao.add_vb(VertexBuffer::create(&cube.texcoords, 2, Primitive::Triangles));
         vao.add_ib(IndexBuffer::create(&cube.indices));
 
-        Mesh {
+        Ok(Mesh {
             vao,
             model_view: Matrix4::one()
-        }
+        })
     }
 
     /// Creates a Mesh object from a Plane
-    pub fn create_plane(plane: Plane) -> Mesh {
-        let mut vao = VertexArrayObject::new();
+    pub fn create_plane(plane: Plane) -> Result<Mesh, RenderError> {
+        let mut vao = VertexArrayObject::new()?;
 
         vao.add_vb(VertexBuffer::create(&plane.vertices, 3, Primitive::Triangles));
         vao.add_vb(VertexBuffer::create(&plane.normals, 3, Primitive::Triangles));
         vao.add_vb(VertexBuffer::create(&plane.texcoords, 2, Primitive::Triangles));
         vao.add_ib(IndexBuffer::create(&plane.indices));
 
-        Mesh {
+        Ok(Mesh {
             vao,
             model_view: Matrix4::one()
-        }
+        })
+    }
+
+    /// Translate this meshh
+    pub fn translate(&mut self, pos: Vector3<f32>) -> &mut Self {
+        self.model_view = self.model_view * Matrix4::from_translation(pos);
+        self
     }
 }
