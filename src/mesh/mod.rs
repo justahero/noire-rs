768a09606d1fedbdfff2a84aaf5b9ec93020f15e
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2019-07-21 18:09:54 +0200

    Add ScreenRect struct to render texture on screen
    This is useful to debug content of textures and render them on screen.

--- src/mesh/mod.rs
+++ src/mesh/mod.rs
@@ -3,9 +3,11 @@ pub use self::mesh::Mesh;
 pub use self::node::Node;
 pub use self::plane::Plane;
 pub use self::scene::Scene;
+pub use self::screen_rect::ScreenRect;
 
 pub mod cube;
 pub mod mesh;
 pub mod node;
 pub mod plane;
 pub mod scene;
+pub mod screen_rect;
