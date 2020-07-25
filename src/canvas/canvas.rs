554b92c534351ffa7e9c7d4c726c1910a1ec7f48
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-07-25 12:52:13 +0200

    Expand draw_line function
    Prepare storing vertices.

--- src/canvas/canvas.rs
+++ src/canvas/canvas.rs
@@ -1,15 +1,48 @@
-use math::{Color, Rect};
+use std::cell::RefCell;
+
+use math::{Color, Rect, Vector2};
+use render::{Program, Shader};
+
+static VERTEX_SHADER: &str = r#"
+#version 330
+
+void main() {
+    gl_Position = vec4(0.0, 0.0, 0.0, 0.1);
+}
+"#;
+
+static FRAGMENT_SHADER: &str = r#"
+#version 330
+
+in vec4 in_color;
+out vec4 out_color;
+
+void main() {
+    out_color = in_color;
+}
+"#;
 
 pub struct Canvas2D {
+    /// compiled shader program to render primitives
+    program: Program,
     /// color to render the next primitive with
     draw_color: Color,
+    /// store all line coordinates
+    line_vertices: RefCell<Box<Vec<Vector2<f32>>>>,
 }
 
 impl Canvas2D {
     /// Create a new instance of the canvas
     pub fn new() -> Self {
+        let vertex_shader = Shader::create_vertex(&VERTEX_SHADER).unwrap();
+        let fragment_shader = Shader::create_fragment(&FRAGMENT_SHADER).unwrap();
+
+        let program = Program::create(vertex_shader, fragment_shader).unwrap();
+
         Canvas2D {
+            program,
             draw_color: Color::BLACK,
+            line_vertices: RefCell::new(Box::new(Vec::new())),
         }
     }
 
@@ -23,8 +56,16 @@ impl Canvas2D {
         self
     }
 
-    /// Draws a single point
-    pub fn draw_point(&self) -> &Self {
+    /// Draws a point
+    pub fn draw_point(&self, x: f32, y: f32) -> &Self {
+        self
+    }
+
+    /// Draws a line
+    pub fn draw_line(&self, start_x: f32, start_y: f32, end_x: f32, end_y: f32) -> &Self {
+        let mut lines = self.line_vertices.borrow_mut();
+        lines.push(Vector2::<f32>{ x: start_x, y: start_y });
+        lines.push(Vector2::<f32>{ x: end_x, y: end_y });
         self
     }
 
@@ -35,6 +76,6 @@ impl Canvas2D {
 
     /// Renders the content of the canvas.
     pub fn render(&self) {
-        // TODO
+        let mut lines = self.line_vertices.borrow_mut();
     }
 }
