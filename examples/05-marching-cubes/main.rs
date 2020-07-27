3ee2cf7ed01f343d2615267f1170fee0598675b8
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-07-28 00:16:36 +0200

    Render graph with lines instead of points

--- examples/05-marching-cubes/main.rs
+++ examples/05-marching-cubes/main.rs
@@ -24,26 +24,34 @@ fn main() {
     let perlin = PerlinNoise::new(10);
 
     let start_time = Instant::now();
+    let increment = 0.02;
 
     loop {
         let now = Instant::now();
         let elapsed = now.duration_since(start_time);
         let elapsed = (elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 * 1e-9) as f32;
 
-        let window_size = window.get_size();
-        let framebuffer_size = window.get_framebuffer_size();
+        let size = window.get_framebuffer_size();
 
         window.reset_viewport();
         window.clear(0.3, 0.3, 0.3, 1.0);
+        canvas.set_color(Color::rgb(1.0, 0.0, 0.0));
 
-        for x in 0..framebuffer_size.width {
-            let xoff = (x as f64) * 0.01;
-            let y = 100.0 + 50.0 * perlin.gen(xoff, 42.3, 3.1);
+        let height: f64  = size.height as f64;
+        let mut xoff = 0.0;
+        let mut last_y = 0.0;
 
-            canvas.draw_point(x as i32, y as i32);
+        for x in 1..size.width {
+            let y = height / 2.0 + height / 2.0 * perlin.gen1(xoff);
+
+            // canvas.draw_point(x as i32, y as i32);
+            canvas.draw_line((x - 1) as i32, last_y as i32, x as i32, y as i32);
+
+            xoff += increment;
+            last_y = y;
         }
 
-        canvas.render(&framebuffer_size);
+        canvas.render(&size);
 
         window.swap_buffers();
 
