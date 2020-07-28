d44a2cb73a2b451b543b6c062777718bac721ba0
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-07-29 00:33:26 +0200

    Try to fix improve 2d perlin noise rendering

--- examples/05-marching-cubes/main.rs
+++ examples/05-marching-cubes/main.rs
@@ -14,7 +14,7 @@ use noire::render::{OpenGLWindow, RenderWindow, Size, Window, Capability};
 use std::time::{Duration, Instant};
 
 fn main() {
-    let window_size = Size::new(640, 640);
+    let window_size = Size::new(800, 800);
     let mut window = RenderWindow::create(&window_size, "Hello This is window")
         .expect("Failed to create Render Window");
 
@@ -24,7 +24,7 @@ fn main() {
     let perlin = PerlinNoise::new(10);
 
     let start_time = Instant::now();
-    let increment = 0.02;
+    let increment = 0.01;
 
     loop {
         let now = Instant::now();
@@ -37,15 +37,17 @@ fn main() {
         window.clear(0.3, 0.3, 0.3, 1.0);
         canvas.set_color(Color::rgb(1.0, 0.0, 0.0));
 
-        let height: f64  = size.height as f64;
-        let mut xoff = 0.0;
+        let increment = 0.04;
+        let mut yoff = 0.5;
 
         // Render 4x4 pixel rects to create less geometry
-        for x in 0..size.width / 4 {
-            for y in 0..size.height / 4 {
+        for y in 0..size.height / 4 {
+            let mut xoff = 0.0;
+
+            for x in 0..size.width / 4 {
                 let index = x + y * size.width;
 
-                let r = perlin.gen1(xoff) as f32;
+                let r = perlin.gen2(xoff, yoff) as f32;
 
                 canvas.set_color(Color::rgb(r, r, r));
 
@@ -56,8 +58,9 @@ fn main() {
                     ((y + 1) * 4) as i32
                 );
 
-                xoff += 0.01;
+                xoff += increment;
             }
+            yoff += increment;
         }
 
         canvas.render(&size);
