eaf2aae1027f493fb78249ec71f424313bd5b718
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-08-27 00:34:34 +0200

    Implement basic version of metaballs
    This renders a number of metaballs with color grading via fragment shader.
    
    * implement a few ops functions to `Vector2` struct
    * fix setting metaballs as uniform variable to shader

--- examples/09-metaballs/main.rs
+++ examples/09-metaballs/main.rs
@@ -17,33 +17,45 @@ use std::time::{Duration, Instant};
 use utils::app_dir;
 use cgmath::Vector3;
 
-const WIDTH: u32 = 640;
-const HEIGHT: u32 = 640;
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 800;
 
-const NUM_BALLS: u32 = 10;
+const NUM_BALLS: u32 = 8;
 const NUM_FRAMES: u32 = 360;
 
 #[derive(Debug)]
 pub struct Ball {
     /// position of the ball
     pub position: Vector2,
-    /// velocity
+    /// Velocity of the ball
     pub velocity: Vector2,
     /// radius of the ball
     pub radius: f32,
 }
 
 impl Ball {
-    pub fn new(x: f32, y: f32, radius: f32) -> Self {
+    pub fn new(x: f32, y: f32, velx: f32, vely: f32, radius: f32) -> Self {
         Self {
             position: Vector2::new(x, y),
-            velocity: Vector2::new(0.0, 0.0),
+            velocity: Vector2::new(velx, vely),
             radius,
         }
     }
 
     pub fn vec3(&self) -> [f32; 3] {
         [self.position.x, self.position.y, self.radius]
+    }
+
+    pub fn update(&mut self) {
+        self.position += self.velocity;
+
+        if self.position.x < 0.0 || self.position.x > WIDTH as f32 {
+            self.velocity.x *= -1.0;
+        }
+
+        if self.position.y < 0.0 || self.position.y > HEIGHT as f32 {
+            self.velocity.y *= -1.0;
+        }
     }
 }
 
@@ -61,7 +73,17 @@ fn main() {
     let mut vao = VertexArrayObject::screen_rect();
 
     let mut balls: Vec<Ball> = (0..NUM_BALLS).into_iter().map(|_| {
-        Ball::new(random_f32(WIDTH as f32), random_f32(HEIGHT as f32), random_f32(10.0))
+        let pos = Vector2::new(random_f32(WIDTH as f32), random_f32(HEIGHT as f32));
+        let mut vel = Vector2::new(-1.0 + random_f32(2.0), -1.0 + random_f32(2.0));
+        vel.normalize();
+        vel *= 1.0 + random_f32(4.0);
+
+        let radius = 8.0 + random_f32(10.0);
+        Ball::new(
+            pos.x, pos.y,
+            vel.x, vel.y,
+            radius,
+        )
     }).collect();
 
     let timer = Timer::now();
@@ -71,26 +93,24 @@ fn main() {
         let elapsed = timer.elapsed_in_seconds() as f32;
 
         fps_timer.next_frame();
-        println!("Frame {} - FPS: {}", fps_timer.total_frames(), fps_timer.fps());
+        // println!("Frame {} - FPS: {}", fps_timer.total_frames(), fps_timer.fps());
 
         window.reset_viewport();
-        window.clear(0.3, 0.3, 0.3, 1.0);
+        window.clear(0.0, 0.0, 0.0, 1.0);
 
-        let size = window.get_framebuffer_size();
+        // animate the points
+        let t = (fps_timer.total_frames() as f32) / (NUM_FRAMES as f32);
+        for ball in &mut balls {
+            ball.update();
+        }
 
+        // render all meta balls
         program.bind();
-        program.uniform("u_resolution", size.into());
-        program.uniform("u_time", elapsed.into());
-
         let vertices = balls
             .iter()
             .map(|b| b.vec3())
             .collect::<Vec<[f32; 3]>>();
-        program.uniform("u_balls", vertices.into());
-
-        // animate the points
-        let t = (fps_timer.total_frames() as f32) / (NUM_FRAMES as f32);
-
+        program.uniform("u_balls[0]", Uniform::Vec3Array(vertices));
 
         vao.bind();
         vao.draw();
--- examples/09-metaballs/shaders/fragment.glsl
+++ examples/09-metaballs/shaders/fragment.glsl
@@ -1,35 +1,32 @@
-#version 330
+#version 420
 
 #define PI 3.14159265359
 
-uniform vec3 u_balls[10];
-
-uniform vec2 u_resolution;
-uniform float u_time;
+uniform vec3 u_balls[8];
 
 out vec4 out_color;
 
-mat2 rotate(float angle) {
-    return mat2(cos(angle), -sin(angle),
-                sin(angle),  cos(angle));
-}
-
-float distance(vec2 left, vec2 right) {
-    return sqrt(left.x * right.x + left.y * right.y);
+// All components are in the range [0…1], including hue.
+// As found on StackOverflow: https://stackoverflow.com/a/17897228
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 }
 
 void main() {
-    vec2 st = (gl_FragCoord.xy / u_resolution.xy) - vec2(0.5);
-    st.x *= u_resolution.x / u_resolution.y;
+    vec2 pixel = gl_FragCoord.xy;
 
     // get distance from pixel to all meta balls
+    float sum = 0.0;
     for (int i = 0; i < u_balls.length; i++) {
-        float d = distance(st, u_balls[i].xy);
+        float d = distance(u_balls[i].xy, pixel);
         float radius = u_balls[i].z;
-
-
+        sum += 5 * radius / d;
     }
 
-    vec3 color = vec3(1.0);
+    sum = clamp(sum, 0, 255);
+
+    vec3 color = hsv2rgb(vec3(sum, 1, 1));
     out_color = vec4(color, 1.0);
 }
