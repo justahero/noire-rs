b1ef40d322f53d4bf94833159a11ddc874efd44d
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-08-29 19:23:38 +0200

    Refactor float vectors to use arrays
    Instead of using Vec of float Vec program enum values use Vec of fixed float arrays, e.g. 2 float components.

--- examples/05-worley-noise/main.rs
+++ examples/05-worley-noise/main.rs
@@ -63,7 +63,12 @@ fn main() {
         program.uniform("u_resolution", size.into());
         program.uniform("u_depth", depth.into());
         program.uniform("u_time", elapsed.into());
-        program.uniform("u_featurePoints[0]", Uniform::Vec3Array(points.clone()));
+        // program.uniform("u_featurePoints[0]", Uniform::Vec3Array(points.clone()));
+        let verts = points
+            .iter()
+            .map(|v| [v.x, v.y, v.z])
+            .collect::<Vec<[f32; 3]>>();
+        program.uniform("u_featurePoints[0]", Uniform::Vec3Array(verts));
 
         vao.bind();
         vao.draw();
