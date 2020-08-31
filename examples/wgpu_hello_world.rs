d08d542f7f39f1c8c1e1b5598686661756fbbc48
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-09-01 00:23:09 +0200

    Add sample to set up main event loop with winit

---
+++ examples/wgpu_hello_world.rs
@@ -0,0 +1,39 @@
+use renderer::{WindowMode, WindowSettings};
+use winit::{event_loop::ControlFlow, event::{WindowEvent, Event}};
+
+extern crate noire;
+
+fn main() {
+    let event_loop = winit::event_loop::EventLoop::new();
+    let settings = WindowSettings::default()
+        .with_title("Test")
+        .with_mode(WindowMode::Windowed);
+
+    let window = settings.create_window(&event_loop);
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = winit::event_loop::ControlFlow::Wait;
+
+        match event {
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            Event::RedrawRequested(_window_id) => {
+                window.request_redraw();
+            }
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput{ ref input, .. } => {
+                    if input.virtual_keycode == Some(winit::event::VirtualKeyCode::Escape) && input.state == winit::event::ElementState::Pressed {
+                        *control_flow = ControlFlow::Exit
+                    }
+                }
+                _ => ()
+            }
+            _ => ()
+        }
+    });
+}
