cb3ad3c66df285f21aba29a824c27a3254164641
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-09-07 00:38:26 +0200

    Add further structs and enums to set up render pass
    This further fills in all options / parameters to set up the render pass. The example code now compiles.
    It adds structs and enums to map to wgpu based versions.

--- src/renderer/src/vertex/mod.rs
+++ src/renderer/src/vertex/mod.rs
@@ -1,3 +1,20 @@
 pub mod vertex;
+pub mod vertex_buffer;
 
 pub use vertex::*;
+pub use vertex_buffer::*;
+
+#[derive(Debug, Copy, Clone)]
+pub enum IndexFormat {
+    Uint16,
+    Uint32,
+}
+
+impl From<IndexFormat> for wgpu::IndexFormat {
+    fn from(format: IndexFormat) -> Self {
+        match format {
+            IndexFormat::Uint16 => wgpu::IndexFormat::Uint16,
+            IndexFormat::Uint32 => wgpu::IndexFormat::Uint32,
+        }
+    }
+}
