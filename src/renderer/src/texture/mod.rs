6c60564b2ca4aed8f28b7153707c4cffd9dd24b7
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-09-09 23:07:26 +0200

    Implement convert function

--- src/renderer/src/texture/mod.rs
+++ src/renderer/src/texture/mod.rs
@@ -57,7 +57,46 @@ pub enum TextureFormat {
 
 impl From<TextureFormat> for wgpu::TextureFormat {
     fn from(format: TextureFormat) -> Self {
-        todo!()
+        match format {
+            TextureFormat::R8Unorm => wgpu::TextureFormat::R8Unorm,
+            TextureFormat::R8Snorm => wgpu::TextureFormat::R8Snorm,
+            TextureFormat::R8Uint => wgpu::TextureFormat::R8Uint,
+            TextureFormat::R8Sint => wgpu::TextureFormat::R8Sint,
+            TextureFormat::R16Uint => wgpu::TextureFormat::R16Uint,
+            TextureFormat::R16Sint => wgpu::TextureFormat::R16Sint,
+            TextureFormat::R16Float => wgpu::TextureFormat::R16Float,
+            TextureFormat::Rg8Unorm => wgpu::TextureFormat::Rg8Unorm,
+            TextureFormat::Rg8Snorm => wgpu::TextureFormat::Rg8Snorm,
+            TextureFormat::Rg8Uint => wgpu::TextureFormat::Rg8Uint,
+            TextureFormat::Rg8Sint => wgpu::TextureFormat::Rg8Sint,
+            TextureFormat::R32Uint => wgpu::TextureFormat::R32Uint,
+            TextureFormat::R32Sint => wgpu::TextureFormat::R32Sint,
+            TextureFormat::R32Float => wgpu::TextureFormat::R32Float,
+            TextureFormat::Rg16Uint => wgpu::TextureFormat::Rg16Uint,
+            TextureFormat::Rg16Sint => wgpu::TextureFormat::Rg16Sint,
+            TextureFormat::Rg16Float => wgpu::TextureFormat::Rg16Float,
+            TextureFormat::Rgba8Unorm => wgpu::TextureFormat::Rgba8Unorm,
+            TextureFormat::Rgba8UnormSrgb => wgpu::TextureFormat::Rgba8UnormSrgb,
+            TextureFormat::Rgba8Snorm => wgpu::TextureFormat::Rgba8Snorm,
+            TextureFormat::Rgba8Uint => wgpu::TextureFormat::Rgba8Uint,
+            TextureFormat::Rgba8Sint => wgpu::TextureFormat::Rgba8Sint,
+            TextureFormat::Bgra8Unorm => wgpu::TextureFormat::Bgra8Unorm,
+            TextureFormat::Bgra8UnormSrgb => wgpu::TextureFormat::Bgra8UnormSrgb,
+            TextureFormat::Rgb10a2Unorm => wgpu::TextureFormat::Rgb10a2Unorm,
+            TextureFormat::Rg11b10Float => wgpu::TextureFormat::Rg11b10Float,
+            TextureFormat::Rg32Uint => wgpu::TextureFormat::Rg32Uint,
+            TextureFormat::Rg32Sint => wgpu::TextureFormat::Rg32Sint,
+            TextureFormat::Rg32Float => wgpu::TextureFormat::Rg32Float,
+            TextureFormat::Rgba16Uint => wgpu::TextureFormat::Rgba16Uint,
+            TextureFormat::Rgba16Sint => wgpu::TextureFormat::Rgba16Sint,
+            TextureFormat::Rgba16Float => wgpu::TextureFormat::Rgba16Float,
+            TextureFormat::Rgba32Uint => wgpu::TextureFormat::Rgba32Uint,
+            TextureFormat::Rgba32Sint => wgpu::TextureFormat::Rgba32Sint,
+            TextureFormat::Rgba32Float => wgpu::TextureFormat::Rgba32Float,
+            TextureFormat::Depth32Float => wgpu::TextureFormat::Depth32Float,
+            TextureFormat::Depth24Plus => wgpu::TextureFormat::Depth24Plus,
+            TextureFormat::Depth24PlusStencil8 => wgpu::TextureFormat::Depth24PlusStencil8,
+        }
     }
 }
 
