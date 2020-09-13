5ae1675d061ec19004bb91bc19bfb0acdf1a5e0a
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-09-14 00:12:19 +0200

    Add function to load shader from file
    * fix vertex shader of spinning cube example

--- examples/01-spinning-cube-wgpu/main.rs
+++ examples/01-spinning-cube-wgpu/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+use renderer::{Renderer, Shader, ShaderStage};
 
+fn main() {
+    let vertex_source = include_str!("shaders/vertex.glsl");
+    let fragment_source = include_str!("shaders/fragment.glsl");
+
+    let renderer = futures::executor::block_on(Renderer::new());
+    let _vertex_shader = renderer.create_shader(vertex_source, ShaderStage::Vertex);
+    let fragment_shader = renderer.create_shader(fragment_source,  ShaderStage::Fragment);
 }
