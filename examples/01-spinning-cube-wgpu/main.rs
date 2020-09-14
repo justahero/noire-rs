1a28e714fd7e3c331cf5c6ad3478f53ecb6bf937
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-09-14 19:16:22 +0200

    Fix fragment shader to compile to Spir-v

--- examples/01-spinning-cube-wgpu/main.rs
+++ examples/01-spinning-cube-wgpu/main.rs
@@ -1,4 +1,4 @@
-use renderer::{Renderer, Shader, ShaderStage};
+use renderer::{Renderer, ShaderLayout, ShaderStage};
 
 fn main() {
     let vertex_source = include_str!("shaders/vertex.glsl");
@@ -7,4 +7,7 @@ fn main() {
     let renderer = futures::executor::block_on(Renderer::new());
     let _vertex_shader = renderer.create_shader(vertex_source, ShaderStage::Vertex);
     let fragment_shader = renderer.create_shader(fragment_source,  ShaderStage::Fragment);
+
+    let shader_layout = ShaderLayout::from_shader(&fragment_shader);
+    dbg!(shader_layout);
 }
