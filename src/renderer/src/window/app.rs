d0b80a7aa4b9f846d8a78b4c7ec4cbcaa65bdf83
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2020-09-20 16:32:18 +0200

    Expand App struct
    * add resources and runner to App

--- src/renderer/src/window/app.rs
+++ src/renderer/src/window/app.rs
@@ -1,18 +1,26 @@
 use std::collections::HashMap;
 
+use resources::Resources;
+
 use winit::window::WindowId as WinitWindowId;
 
 use crate::Window;
 
 pub struct App {
+    /// The runner
+    runner: Box<dyn Fn(App)>,
     /// Lookup table to find find Window by WindowId
     pub windows: HashMap<WinitWindowId, Window>,
+    /// The list of resources
+    pub resources: Resources,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
+            runner: Box::new(run_once),
             windows: HashMap::new(),
+            resources: Resources::new(),
         }
     }
 }
@@ -27,4 +35,28 @@ impl App {
     pub fn update(&mut self) {
         // TODO
     }
+
+    /// Builds a new App
+    pub fn build() -> Self {
+        Default::default()
+    }
+
+    /// Adds a new resource
+    pub fn add_resource<T>(&mut self, value: T) -> &mut Self
+    where
+        T: Send + Sync + 'static,
+    {
+        self.resources.insert::<T>(value);
+        self
+    }
+
+    /// Executes the runner
+    pub fn run(mut self) {
+        let runner = std::mem::replace(&mut self.runner, Box::new(run_once));
+        (runner)(self);
+    }
+}
+
+fn run_once(mut app: App) {
+    app.update();
 }
